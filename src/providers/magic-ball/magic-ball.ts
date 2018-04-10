ae2c54b6e1c4899a18955849cfa6432fd1ebbdbf
Yong Li <dev@liyong.cn>
2018-04-10 13:43:46 +0100

    add business logics

--- src/providers/magic-ball/magic-ball.ts
+++ src/providers/magic-ball/magic-ball.ts
@@ -10,8 +10,34 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class MagicBallProvider {
 
-  constructor(public http: HttpClient) {
-    console.log('Hello MagicBallProvider Provider');
+  answers: any;
+ 
+  constructor(){
+ 
+    this.answers = [
+      'Yes',
+      'No',
+      'Maybe',
+      'All signs point to yes',
+      'Try again later',
+      'Without a doubt',
+      'Don\'t count on it',
+      'Most likely',
+      'Absolutely not'
+    ];
+ 
+  }
+ 
+  getAnswers(){
+    return this.answers;
+  }
+ 
+  getRandomAnswer(){
+    return this.answers[this.getRandomInt(0, this.answers.length-1)];
+  }
+ 
+  getRandomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
 }
